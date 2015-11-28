22010c97e4d3d737eb4b9c9774ed31a23a595236
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2015-11-28 04:35:43 +0100

    List of objects implementing players interfaces

--- VanDerWaerdenGame.DesktopApp/MainWindowViewModel.cs
+++ VanDerWaerdenGame.DesktopApp/MainWindowViewModel.cs
@@ -15,21 +15,34 @@ namespace VanDerWaerdenGame.DesktopApp
     {
         public MainWindowViewModel() : base()
         {
+            //Player is loaded just to have static linking ot the Players assembly
 
+            var plr = new RandomColorPlayer();
+            
+            ColorPlayers = AppDomain.CurrentDomain.GetAssemblies()
+                        .SelectMany(s => s.GetTypes())
+                        .Where(p => typeof(IColorPlayer).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
+                        .Select(t => (IColorPlayer)Activator.CreateInstance(t))
+                        .ToList();
+
+            PositionPlayers = AppDomain.CurrentDomain.GetAssemblies()
+                        .SelectMany(s => s.GetTypes())
+                        .Where(p => typeof(IPositionPlayer).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
+                        .Select(t => (IPositionPlayer)Activator.CreateInstance(t))
+                        .ToList();
+            GameManager.Player1 = PositionPlayers.Last();
+            GameManager.Player2 = ColorPlayers.Last();
         }
+
+        public List<IColorPlayer> ColorPlayers { get; set; }
+        public List<IPositionPlayer> PositionPlayers { get; set; }
 
         public GameManager GameManager { get { return gameManager; } set { SetProperty(ref gameManager, value); } }
-        private GameManager gameManager = new GameManager(new VanDerWaerdenGameRules()) { Player1 = new RandomPositionPlayer(), Player2 = new RandomColorPlayer() };
+        private GameManager gameManager = new GameManager(new VanDerWaerdenGameRules());
 
-        public void StartNewGame()
-        {
-            this.GameManager.NewGame();
-        }
+        public void StartNewGame() { this.GameManager.NewGame(); }
 
-        public void Turn()
-        {
-            this.GameManager.IterateTurn();
-        }
+        public void Turn() { this.GameManager.IterateTurn(); }
 
         public void PlayTillEnd() { this.GameManager.PlayTillEnd(); }
 
